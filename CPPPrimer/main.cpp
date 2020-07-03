20beae8d1ec3aa6c7bedff446ff5b1c26494cbed
Sanjay <sanjayp@g.clemson.edu>
2020-07-02 17:08:21 -0700

    Initial Commit

---
+++ CPPPrimer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPPPrimer
+//
+//  Created by Sanjay Patil on 7/2/20.
+//  Copyright © 2020 Sanjay Patil. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
